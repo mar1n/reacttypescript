ecbb0222cda242fbfe0d4be692c20064816ca1d3
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-10 23:35:29 +0100

    stage 2 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
-import './App.css';
-import { TextField } from './TextField';
+import React, { useState, useEffect } from "react";
+interface list {
+  id: number;
+  name: string;
+}
 
 const App: React.FC = () => {
+  const [search, setSearch] = useState<string>('');
+  const [list, setList] = useState<list[]>([]);
+  const [data, setData] = useState<list[]>([]);
+  useEffect(() => {
+    async function szymon() {
+      try {
+        let fetchCall = await fetch("https://api.github.com/users/Jonjoe/repos");
+        let data = await fetchCall.json();
+        setData(data);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    szymon();
+  }, []);
+
+  const searchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      let search = data.filter(word => word.name.includes(e.target.value))
+      setSearch(e.target.value)
+      setList(search)
+  }
   return (
     <div className="App">
-      <TextField  text='hello'  />
+      <p>hello world</p>
+      <p>{search}</p>
+      <input value={search} onChange={searchChange} />
+      {list.map((details) => (
+        <div key={details.id} className="details">{details.name}</div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
