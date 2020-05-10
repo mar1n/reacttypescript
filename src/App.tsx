44b3658cd0718ad558478c76d5798b0847e38bee
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-10 23:40:11 +0100

    small fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,34 @@ const App: React.FC = () => {
   const [list, setList] = useState<list[]>([]);
   const [data, setData] = useState<list[]>([]);
   useEffect(() => {
-    async function szymon() {
+    async function getData() {
       try {
-        let fetchCall = await fetch("https://api.github.com/users/Jonjoe/repos");
+        let fetchCall = await fetch(
+          "https://api.github.com/users/Jonjoe/repos"
+        );
         let data = await fetchCall.json();
         setData(data);
       } catch (e) {
         console.log(e);
       }
     }
-    szymon();
+    getData();
   }, []);
 
   const searchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      let search = data.filter(word => word.name.includes(e.target.value))
-      setSearch(e.target.value)
-      setList(search)
-  }
+    let search = data.filter((word) => word.name.includes(e.target.value));
+    setSearch(e.target.value);
+    setList(search);
+  };
   return (
     <div className="App">
       <p>hello world</p>
       <p>{search}</p>
       <input value={search} onChange={searchChange} />
       {list.map((details) => (
-        <div key={details.id} className="details">{details.name}</div>
+        <div key={details.id} className="details">
+          {details.name}
+        </div>
       ))}
     </div>
   );
